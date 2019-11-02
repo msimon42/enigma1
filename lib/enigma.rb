1fa7a425438ed64b8ffad8217e01678a97807d17
msimon42 <msimon@uab.edu>
2019-11-02 13:28:14 -0600

    Add implementation code

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Enigma
   attr_reader :char_set
   def initialize
@@ -12,7 +13,17 @@ class Enigma
 
   end
 
-  def shift(phrase, key=nil, date=nil)
+  def get_date_shift(date)
+    squared = date.to_i * date.to_i
+    shift_arr = squared.to_s.split(//).map{|num| num.to_i}.last(4)
+  end
 
-  end       
+  def get_raw_keys(key, date)
+    split_key = Array.new
+    raw_keys = Array.new
+    key.split(//).map{|num| num.to_i}.each_cons(2){|num_set| split_key << num_set}
+    keys = split_key.map do |num_set|
+      num_set.join.to_i
+    end
+  end
 end
