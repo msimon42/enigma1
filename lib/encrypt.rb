71499a17c7f1291a031423462a2766d4e4143c99
msimon42 <msimon@uab.edu>
2019-11-03 17:54:47 -0700

    Add method for gathering user input

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,6 +4,12 @@ class Encrypt
   end
 
   def user_input
-
-  end     
+    puts 'Please enter a filename: '
+    @filename = gets.chomp
+    puts "If you would like to provide a key, please do so here. Otherwise type 'skip'."
+    @key = gets.chomp
+    puts "Please enter a date, or type 'skip' to use today's date."
+    @date = gets.chomp
+  end
+    
 end
