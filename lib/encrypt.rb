5853c708b35cff918e19e0e511a0a77664058dea
msimon42 <msimon@uab.edu>
2019-11-04 17:49:21 -0700

    Refactor encrypt class

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,37 +1,13 @@
-require 'pry'
+
 require_relative 'enigma'
 class Encrypt
   attr_reader :key
-  def initialize
+
+  def self.encrypt
     @enig = Enigma.new
-  end
-
-  def user_input
-    puts 'Please enter a filename that contains your message: '
-    @filename = gets.chomp
-    puts 'Please enter a filename to save your encrypted message: '
-    @outfilename = gets.chomp
-  end
-
-  def generate_key
-    KeyGenerator.generate
-  end
-
-  def generate_date
-    DateGenerator.generate
-  end
-
-  def encrypt
-    @output = @enig.encrypt(@phrase, @key, @date)
-  end
-
-  def self.run
-    new = self.new
-    new.user_input
-    new.read_file
-    new.encrypt
-    new.write_to_file
+    file_names = FileReader.user_input
+    file_data = FileReader.read_file(file_names[0])
+    output = @enig.encrypt(file_data[0], file_data[1], file_data[2])
+    FileReader.write_to_file(file_names[1], output)
   end
 end
-
-Encrypt.run
