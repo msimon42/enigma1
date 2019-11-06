787a3fbfc2bc2d4c310ed0545458df2031bafc3b
msimon42 <msimon@uab.edu>
2019-11-06 08:30:21 -0700

    Adds command line confirmation

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -7,5 +7,6 @@ class Decryptor
     file_data = FileReader.read_file(file_names[0])
     output = @enig.decrypt(file_data[0], file_data[1], file_data[2])
     FileReader.write_to_file(file_names[1], output.values)
+    puts "#{file_names[0]} has been decrypted and saved to #{file_names[1]}"
   end
 end
--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -7,5 +7,6 @@ class Encryptor
     file_data = FileReader.read_file(file_names[0])
     output = @enig.encrypt(file_data[0], file_data[1], file_data[2])
     FileReader.write_to_file(file_names[1], output.values)
+    puts "#{file_names[0]} has been encrypted and saved to #{file_names[1]}"
   end
 end
